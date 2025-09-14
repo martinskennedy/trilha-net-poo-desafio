360984a9fba0862b7b4bfc19c286884e59882fe9
Kennedy Martins <kemartins@gmail.com>
2025-09-14 11:29:05 -0300

    Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
+using System.Diagnostics.CodeAnalysis;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+
+Console.WriteLine("Smartphone Nokia:");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+
+Iphone iphone = new Iphone("987654321", "iPhone 13", "987654321098765", 128);
+
+Console.WriteLine("Smartphone iPhone:");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
